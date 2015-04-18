af3ad21242130b92980cb865233e71a1d1a66e86
Andrew Hautau <arhautau@gmail.com>
2015-04-18 16:06:44 -0700

    ui elements and auto layout constraints

---
+++ UW_HW02_arhautau/ModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModalViewController.h
+//  UW_HW02_arhautau
+//
+//  Created by Andrew Hautau on 4/18/15.
+//  Copyright (c) 2015 Andrew Hautau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+
+@end
