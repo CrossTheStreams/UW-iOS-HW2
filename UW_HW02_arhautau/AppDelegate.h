c71646ae9b05f1457e8c45050e208b0a93bf5d7e
Andrew Hautau <arhautau@gmail.com>
2015-04-18 12:34:14 -0700

    Initial Commit

---
+++ UW_HW02_arhautau/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UW_HW02_arhautau
+//
+//  Created by Andrew Hautau on 4/18/15.
+//  Copyright (c) 2015 Andrew Hautau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
